dceb5db4c9206c0a79e9e34a699d635f98c4ea44
Johannkrusa <Johannkrusa@gmail.com>
2024-05-10 15:14:20 +0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,125 @@
+// ============ exercise ============
+
+// query 1 
+// Create a function to check if two objects are equal
+
+function equalObject(obj1, obj2){
+    var keys1 = Object.keys(obj1);
+    var keys2 = Object.keys(obj2);
+
+    console.log(keys1, keys2);
+
+    if (keys1.length !== keys2.length) {
+        return false; 
+    }
+    for (var i of keys1){
+        if(obj1[i] !== obj2[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+var obj1 = { a : 1};
+var obj2 = { a : 1};
+
+console.log(equalObject(obj1,obj2));
+
+// query 2 
+// ● Create a function to get the intersection of two objects
+// ● Example
+// ○ Input : { a: 1, b: 2 } & { a: 1, c: 3 }
+// ○ Output: { a: 1 }
+
+
+function intersectionObject(obj1, obj2) {
+    var keys1 = Object.keys(obj1);
+    var keys2 = Object.keys(obj2);
+    var tempObj = {};
+
+    for (var item of keys1) {
+        if(obj1[item] === obj2[item]){
+            tempObj[item] = obj1[item];
+        }
+    }
+
+    return tempObj;
+}
+
+var obj1 = {a:1, b:2}
+var obj2 = {a:1, c:3}
+
+console.log(intersectionObject(obj1, obj2));
+
+// query 3
+// Exercise
+// ● Create a function to merge two array of student data and remove duplicate data
+// ● Student data : name & email
+
+function removeDuplicate(data1, data2) {
+    var tempData = data1.concat(data2);
+    var nonDuplicateData = [];
+
+    for (var key in tempData) {
+        var student = tempData[key];
+        var found = false;
+        for (var i = 0; i <= nonDuplicateData.length -1 ; i++) {
+            if (nonDuplicateData[i].email === student.email) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            nonDuplicateData.push(student);
+        }
+    }
+
+    return nonDuplicateData;
+}
+
+var Data1 = [
+    { name: "Student 1", email: "student1@mail.com" },
+    { name: "Student 2", email: "student2@mail.com" }
+]
+
+var Data2 = [
+    { name: "Student 1", email: "student1@mail.com" },
+    { name: "Student 3", email: "student3@mail.com" }
+]
+
+console.log(removeDuplicate(Data1, Data2));
+
+// query 4
+// skip
+
+function switchKey(data){
+    var result = [];
+    for (let item of data){
+        var  newObj = {};
+        for(let key in item){
+            newObj[item[key]] = key;
+        }
+        result.push(newObj)
+    }
+    return result;
+}
+
+var data = [{name:"Luna", age:20}, {name:"noah", age: 41}];
+console.log(data);
+console.log(switchKey(data));
+
+
+// query 5
+
+
+
+function factorial(n) { 
+    if (n <= 1) { 
+        return 1; 
+    } else { 
+        return n * factorial(n - 1); 
+    } 
+} 
+console.log(factorial(6));
